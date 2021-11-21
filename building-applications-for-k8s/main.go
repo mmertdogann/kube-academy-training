c526b4894be91b761922a865e6e84aa705ece7df
mmertdogann <mm3rtdogann@gmail.com>
2021-11-21 23:32:46 +0300

    Update files first time

---
+++ building-applications-for-k8s/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	log.Println("Starting: Building Apps For K8s app")
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	log.Printf("Request received from %s", r.RemoteAddr)
+	fmt.Fprint(w, "Building Apps For K8s app says Hi")
+}
